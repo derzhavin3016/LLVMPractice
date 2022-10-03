f17b6e4dd634863864fc150dd9a3ebad6c572533
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-10-03 21:32:45 +0300

    [life] Small fixes
    - Changed window position at create
    - Remove redundant typenames

--- Life/graph.cc
+++ Life/graph.cc
@@ -7,6 +7,7 @@ static sf::RenderWindow MainWindow{};
 void createWindow(std::uint32_t width, std::uint32_t height)
 {
   MainWindow.create(sf::VideoMode(width, height), "Game of Life");
+  MainWindow.setPosition({100, 100});
 }
 
 bool isWindowOpen()
@@ -18,13 +19,14 @@ void handleEvents()
 {
   sf::Event event;
   while (MainWindow.pollEvent(event))
-    if (event.type == sf::Event::Closed)
+    if (event.type == sf::Event::Closed ||
+        (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
       MainWindow.close();
 }
 
 void clearWindow(std::uint8_t r, std::uint8_t g, std::uint8_t b)
 {
-  MainWindow.clear(sf::Color(r, g, b));
+  MainWindow.clear({r, g, b});
 }
 
 void flushWindow()
@@ -36,7 +38,7 @@ void putPixel(std::uint32_t x, std::uint32_t y, std::uint8_t r, std::uint8_t g, 
 {
   auto coord = sf::Vector2f(x, y);
 
-  sf::Vertex point(coord, sf::Color(r, g, b));
+  sf::Vertex point(coord, {r, g, b});
   MainWindow.draw(&point, 1, sf::Points);
 }
 } // namespace graph

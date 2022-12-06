4c175d51af19961394fd4c75701d69e33f0349a6
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-12-06 12:00:17 +0300

    [lang] Add ret in case of parsing failure

--- Lang/main.cc
+++ Lang/main.cc
@@ -7,7 +7,10 @@ int main()
   yy::Driver driver(std::cin, std::cout);
 
   if (!driver.parse())
+  {
     std::cout << "Parsing failed" << std::endl;
+    return 1;
+  }
 
   driver.codegen();
   driver.dumpIR(std::cout);
